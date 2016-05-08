4426f717bface7f05de583c316b81102959b0045
Chris Boette <cboette@gmail.com>
2016-05-08 16:47:56 -0500

    Add React. Display counts of survey results in admin view of surveys template.

---
+++ public/javascripts/components.js
@@ -0,0 +1,60 @@
+var isNode = typeof module !== 'undefined' && module.exports;
+var React = isNode ? require('react') : window.React;
+var ReactDOM = isNode ? require('react-dom') : window.ReactDOM;
+var _ = isNode ? require('lodash') : window._;
+
+var SurveyResults = React.createClass({
+  getInitialState: function() {
+    return {
+      results: []
+    };
+  },
+
+  loadServerData: function() {
+    $.get('/api/surveys', function(results) {
+      if (this.isMounted()) {
+        this.setState({ results: results.surveyResults });
+      }
+    }.bind(this))
+  },
+
+  componentDidMount: function() {
+    this.loadServerData();
+    this.intervalId = setInterval(this.loadServerData, 3000);
+  },
+
+  componentWillUnmount: function() {
+    clearInterval(this.intervalId)
+  },
+
+  render: function() {
+    var results = this.state.results
+      ? this.state.results
+      : this.props.results;
+    return (
+      <ul>
+        {_.map(results, function(result, index) {
+          return <li key={index}>
+            <p>{result.question}</p>
+            <ul>
+              {_.map(result.counts, function(ct, index) {
+                return <li key={'ct=' + index}>
+                  {ct.response} | {ct.count}
+                </li>
+              })}
+            </ul>
+          </li>
+        })}
+      </ul>
+    );
+  }
+});
+
+if (isNode) {
+  exports.SurveyResults = SurveyResults;
+} else {
+  ReactDOM.render(
+    <SurveyResults results='[]' />,
+    document.getElementById('results')
+  );
+}
--- routes/surveys.js
+++ routes/surveys.js
@@ -1,11 +1,18 @@
+var React = require('react');
+var ReactDOM = require('react-dom/server');
+
+var components = require('../public/javascripts/components');
+var SurveyResults = React.createFactory(components.SurveyResults);
+
 var isAdmin = require('../modules/auth').isAdmin;
 var isLoggedIn = require('../modules/auth').isLoggedIn;
 
 module.exports = function(router) {
   router.get('/surveys', isLoggedIn, function(req, res) {
     res.render('survey', {
-      user: req.user,
-      message: req.flash('surveyMessage')
+      message: req.flash('surveyMessage'),
+      react: ReactDOM.renderToString(SurveyResults()),
+      user: req.user
     });
   });
 
